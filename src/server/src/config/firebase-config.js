a0d3b5be4a17a91c303a86636ea2ec8bb613deea
renad <120721701+Renadyousef@users.noreply.github.com>
2025-09-02 16:34:38 +0300

    firebase connected and DB created

--- src/server/src/config/firebase-config.js
+++ src/server/src/config/firebase-config.js
@@ -6,4 +6,31 @@ admin.initializeApp({
     credential:admin.credential.cert(serviceAccount)
 })
 
-module.exports=admin;
+console.log("firebase Admin initialized!")
+
+//1.get a DB instance connection
+const DB=admin.firestore()
+
+//2 create colloctions
+const Students=DB.collection('students')
+const Task=DB.collection('task')
+const StudyPlans=DB.collection('study_plans')
+const FlashCards=DB.collection('flash_cards')
+const Quizzes=DB.collection('quizzes')
+const Questions=DB.collection('questions')
+const FeedBack=DB.collection('feed_back')
+const QuizResult=DB.collection('quiz_result')
+const StudySession=DB.collection('study_session')
+
+
+module.exports = {
+    Students,
+    Task,
+    StudyPlans,
+    FlashCards,
+    Quizzes,
+    Questions,
+    FeedBack,
+    QuizResult,
+    StudySession
+};
--- src/server/src/server.js
+++ src/server/src/server.js
@@ -1,9 +1,25 @@
 const express=require('express');
 
+const {
+  Students,
+  Task,
+  StudyPlans,
+  FlashCards,
+  Quizzes,
+  Questions,
+  FeedBack,
+  QuizResult,
+  StudySession
+} = require('./config/firebase-config'); // your exported collections
+
 const app=express()
 const PORT=5000;
 
 //app routes
-app.get('/',(req,res)=>{
+app.post('/',(req,res)=>{
     res.send("welcome abroed")
 })
+
+
+
+app.listen(PORT,()=>{console.log('server running on port 5000')})
