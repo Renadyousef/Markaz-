e54f4edea314c2af3cb0d3c75b1d3025ce260da9
renad <120721701+Renadyousef@users.noreply.github.com>
2025-09-03 15:05:53 +0300

    Update firebase-config.js

--- src/server/src/config/firebase-config.js
+++ src/server/src/config/firebase-config.js
@@ -1,36 +1,39 @@
-const admin=require('firebase-admin');
-require('dotenv').config(); // Load .env variables
-const serviceAccount=require(process.env.SERVICE_ACCOUNT_PATH);//please download a copy on same path !
+const admin = require('firebase-admin');
+const path = require('path');
+
+// Load .env from server folder
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+// Load the service account JSON
+const serviceAccountPath = path.resolve(__dirname, 'serviceAccount.json');
+const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
-    credential:admin.credential.cert(serviceAccount)
-})
+  credential: admin.credential.cert(serviceAccount)
+});
 
-console.log("firebase Admin initialized!")
+console.log("firebase Admin initialized!");
 
-//1.get a DB instance connection
-const DB=admin.firestore()
-
-//2 create colloctions
-const Students=DB.collection('students')
-const Task=DB.collection('task')
-const StudyPlans=DB.collection('study_plans')
-const FlashCards=DB.collection('flash_cards')
-const Quizzes=DB.collection('quizzes')
-const Questions=DB.collection('questions')
-const FeedBack=DB.collection('feed_back')
-const QuizResult=DB.collection('quiz_result')
-const StudySession=DB.collection('study_session')
-
+// Firestore collections
+const DB = admin.firestore();
+const Students = DB.collection('students');
+const Task = DB.collection('task');
+const StudyPlans = DB.collection('study_plans');
+const FlashCards = DB.collection('flash_cards');
+const Quizzes = DB.collection('quizzes');
+const Questions = DB.collection('questions');
+const FeedBack = DB.collection('feed_back');
+const QuizResult = DB.collection('quiz_result');
+const StudySession = DB.collection('study_session');
 
 module.exports = {
-    Students,
-    Task,
-    StudyPlans,
-    FlashCards,
-    Quizzes,
-    Questions,
-    FeedBack,
-    QuizResult,
-    StudySession
+  Students,
+  Task,
+  StudyPlans,
+  FlashCards,
+  Quizzes,
+  Questions,
+  FeedBack,
+  QuizResult,
+  StudySession
 };
