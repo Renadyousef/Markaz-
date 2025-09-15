04c636180c030ede998e8cab894b43c59c9ed50f
renad <120721701+Renadyousef@users.noreply.github.com>
2025-09-15 14:59:07 +0300

    added footer to auth page

--- src/client/src/components/auth/AuthPage.jsx
+++ src/client/src/components/auth/AuthPage.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
+import Footer from '../Header_Footer/Footer'
 
 export default function AuthPage({ setToken, initialTab = "signin" }) {
   const [activeTab, setActiveTab] = useState(initialTab);
@@ -11,7 +12,7 @@ export default function AuthPage({ setToken, initialTab = "signin" }) {
   }, [initialTab]);
 
   return (
-    <div className="auth-container">
+   <div className="after_footer">  <div className="auth-container">
       <div className="auth-tabs">
         <button
           className={activeTab === "signin" ? "tab active" : "tab"}
@@ -31,6 +32,9 @@ export default function AuthPage({ setToken, initialTab = "signin" }) {
         {activeTab === "signin" ? <SignIn setToken={setToken} /> : <SignUp setToken={setToken} />}
        
       </div>
+      
     </div>
+    <Footer/></div>
+   
   );
 }
