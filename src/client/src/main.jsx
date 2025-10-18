bea83404cd09bf230705c01a551f8f5de71a7172
renad <120721701+Renadyousef@users.noreply.github.com>
2025-10-18 17:33:45 +0300

    packges

--- src/client/src/main.jsx
+++ src/client/src/main.jsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 import './config/firebase-config.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom'; // ✅ import BrowserRouter
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>  {/* ✅ wrap App in Router */}
